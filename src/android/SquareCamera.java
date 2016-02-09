f24caf7edf453cb7f3d605a7d07dfdcab1976f3f
Santtu Lintervo <santervo@gmail.com>
2016-02-09 10:33:23 +0200

    Added support for pictureMinWidth and storageDirectory parameters

--- src/android/SquareCamera.java
+++ src/android/SquareCamera.java
@@ -8,6 +8,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.json.JSONException;
 
 import com.desmond.squarecamera.CameraActivity;
@@ -17,20 +18,29 @@ public class SquareCamera extends CordovaPlugin {
 
   private CallbackContext callbackContext;
 
+  private static final String PICTURE_MIN_WIDTH = "pictureMinWidth";
+  private static final String STORAGE_DIRECTORY = "storageDirectory";
+
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     this.callbackContext = callbackContext;
 
     if (action.equals("getPicture")) {
-      getPicture();
+      getPicture(args.getJSONObject(0));
       return true;
     }
 
     return false;
   }
 
-  public void getPicture() {
-    Intent startCustomCameraIntent = new Intent(this.cordova.getActivity(), CameraActivity.class);
-    this.cordova.startActivityForResult(this, startCustomCameraIntent, REQUEST_CAMERA);
+  public void getPicture(JSONObject opts) throws JSONException {
+    Intent intent = new Intent(this.cordova.getActivity(), CameraActivity.class);
+    if(!opts.isNull(PICTURE_MIN_WIDTH)) {
+      intent.putExtra(CameraActivity.EXTRA_PICTURE_MIN_WIDTH, opts.getInt(PICTURE_MIN_WIDTH));
+    }
+    if(!opts.isNull(STORAGE_DIRECTORY)) {
+      intent.putExtra(CameraActivity.EXTRA_STORAGE_DIRECTORY, opts.getString(STORAGE_DIRECTORY));
+    }
+    this.cordova.startActivityForResult(this, intent, REQUEST_CAMERA);
   }
 
   public void onActivityResult(int requestCode, int resultCode, Intent intent) {
--- www/square-camera.js
+++ www/square-camera.js
@@ -1,5 +1,19 @@
 var exec = require('cordova/exec')
 
-exports.getPicture = function(success, error) {
-  exec(success, error, "SquareCamera", "getPicture", []);
+function extend(o1, o2) {
+  for (var i in o2) {
+    if (o2.hasOwnProperty(i)) {
+      o1[i] = o2[i];
+    }
+  }
+  return o1
+}
+
+exports.getPicture = function(success, error, opts) {
+  opts = extend({
+    pictureMinWidth: null,
+    storageDirectory: "SquareCamera"
+  }, opts || {})
+
+  exec(success, error, "SquareCamera", "getPicture", [opts]);
 }
